d2a72aa630bc66db35bf4af0ac7324ce644c94e6
luan menezesds <luanmenezesds615@gmail.com>
2026-02-05 18:11:54 -0300

    interceptors

---
+++ src/common/interceptors/logger.interceptor.ts
@@ -0,0 +1,23 @@
+import { ExecutionContext, NestInterceptor, CallHandler, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Request } from 'express'; 
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>(); 
+    const method = request.method;
+    const url = request.url;
+    const now = Date.now();
+
+    console.log(`[REQUEST] ${method} ${url} - início da req`);
+
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - now;
+        console.log(`[RESPONSE] ${method} ${url} - ${duration}ms`);
+      }),
+    );
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseInterceptors } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "src/tasks/dto/create-task.dto";
 import { UpdateTaskDto } from "src/tasks/dto/update-task.dto";
 import { PaginationDto } from "src/common/dto/pagination.dto";
+import { LoggerInterceptor } from "src/common/interceptors/logger.interceptor";
+import { BodyCreateTaskInterceptor } from "src/common/interceptors/body-create-task.interceptor";
+import { AddHeaderInterceptor } from "src/common/interceptors/add-header.interceptor";
 
 @Controller('tasks')
 export class TasksController{
   constructor(private readonly tasksService: TasksService){}
 
   @Get()
+  @UseInterceptors(LoggerInterceptor)
+  @UseInterceptors(AddHeaderInterceptor)
 findAllTask(@Query() paginationDto: PaginationDto){
   return this.tasksService.findAll(paginationDto)
 }
@@ -19,6 +24,7 @@ findOneTask(@Param('id', ParseIntPipe) id: number){
 }
 
 @Post()
+@UseInterceptors(BodyCreateTaskInterceptor)
 createTasks(@Body() createTaskDto: CreateTaskDto ){
   return this.tasksService.create(createTaskDto)
 }
